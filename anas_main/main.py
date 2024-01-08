062690bbc6f000bd7de84543b039d2a7aacd5b39
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-01-08 19:41:01 +0200

    08/1/2024

--- anas_main/main.py
+++ anas_main/main.py
@@ -1,12 +1,13 @@
 import message,app
 import telegram
 from telegram import InlineKeyboardMarkup,InlineKeyboardButton
-from telegram.ext import CommandHandler,MessageHandler,filters,ApplicationBuilder
+from telegram.ext import CommandHandler,MessageHandler,filters,ApplicationBuilder,CallbackQueryHandler
 with open("token.bot","r",encoding="utf-8") as file:
     bot=ApplicationBuilder().token(file.read()).build()
 async def start(update,contextt):
     info=update.effective_user
-    await message.Sendmessage(chat_id=info.id,text="welcome " + str(info.first_name) + "to this bot. ")
+    keyboard=InlineKeyboardMarkup([[InlineKeyboardButton("donate",url="https://www.paypal.me/AMohammed231")],[InlineKeyboardButton("help",callback_data="help")]])
+    await message.Sendmessage(chat_id=info.id,text="welcome " + str(info.first_name) + "to this bot. ",reply_markup=keyboard)
 async def helb(update,contextt):
     links="""<a href="https://t.me/mesteranasm">telegram</a>
 
@@ -23,7 +24,14 @@ anasformohammed@gmail.com
 """
     info=update.effective_user
     await message.Sendmessage(info.id,"""name: {}\nversion: {}\ndescription: {}\n developer: {}\n contect us {}""".format(app.name,str(app.version),app.description,app.developer,links))
+async def callBake(update,contextt):
+    q=update.callback_query
+    q.answer()
+    if q.data=="help":
+        await helb(update,contextt)
+
 print("running")
 bot.add_handler(CommandHandler("start",start))
 bot.add_handler(CommandHandler("help",helb))
+bot.add_handler(CallbackQueryHandler(callBake))
 bot.run_polling()
